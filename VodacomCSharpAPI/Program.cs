ca64a5212e5b91d3a7e100534800479ea9249094
Farisani-Ratshikombo-BBD <farisanipaul.ratshikombo@vcontractor.co.za>
2024-06-27 13:57:57 +0200

    Adding Cors

--- VodacomCSharpAPI/Program.cs
+++ VodacomCSharpAPI/Program.cs
@@ -6,11 +6,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseCors(
+    options => options.WithOrigins("https://static-miniapp.onrender.com").AllowAnyMethod()
+);
 
 app.UseAuthorization();
 
--- VodacomCSharpAPI/obj/Debug/net8.0/VodacomCSharpAPI.AssemblyInfo.cs
+++ VodacomCSharpAPI/obj/Debug/net8.0/VodacomCSharpAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VodacomCSharpAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d743dab119441d338686a4a9e3c3af65f284515d")]
 [assembly: System.Reflection.AssemblyProductAttribute("VodacomCSharpAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VodacomCSharpAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
