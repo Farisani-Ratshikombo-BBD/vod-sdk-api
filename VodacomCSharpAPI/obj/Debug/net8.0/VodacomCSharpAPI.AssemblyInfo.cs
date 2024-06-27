96151869bd0adfb2e36404d51a8d8545c9d6c290
Farisani-Ratshikombo-BBD <farisanipaul.ratshikombo@vcontractor.co.za>
2024-06-27 16:04:56 +0200

    klsdajlksdfaklafsdk

--- VodacomCSharpAPI/obj/Debug/net8.0/VodacomCSharpAPI.AssemblyInfo.cs
+++ VodacomCSharpAPI/obj/Debug/net8.0/VodacomCSharpAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VodacomCSharpAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511356015839f57c88bceb30a6d86bbc22f22bec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19fbbf7853800ac80bc83aeb4814f999c50af94b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VodacomCSharpAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VodacomCSharpAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
